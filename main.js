aa9eb9b5a15db9880998df6ecd3704ccf3642b2b
Vasishth Gupta <vasishthemail@gmail.com>
2021-10-08 22:13:58 +0530

    e

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 
+// function getEed(){
+//     alert ("Use fullscreen for the best expirience");
+// }
+function closeWindow(){
+}
+    var elem = document.getElementById("maxwindow");
+    var elem = document.documentElement;
+
+    /* View in fullscreen */
+    function openFullscreen() {
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+      } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+      }
+    }
