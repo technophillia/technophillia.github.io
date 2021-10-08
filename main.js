414bce6f7ea8a24c054744c2acfc7663af288225
Vasishth Gupta <vasishthemail@gmail.com>
2021-10-08 09:56:21 +0530

    r

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ function getEed(){
     alert ("use fullscreen for the best expirience");
     stop()
 }
+function closeWindow(){
+    window.close();
+setTimeout(() => {window.close();}, 2000);
+setTimeout(() => {window.close();}, 4000);
+}
