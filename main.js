18eaf8b6fa760ef40d66b92b1335a3d984201a8c
Vasishth Gupta <vasishthemail@gmail.com>
2021-10-12 15:17:41 +0530

    Update

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function closeWindow(){
+}
+    var elem = document.getElementById("maxwindow");
+    var elem = document.documentElement;
+
+    /* View in fullscreen */
+    function openFullscreen() {
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+      } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+      }
+    }
